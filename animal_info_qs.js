4eea45a08397570a5dfedd90e9485a61ccf5eea8
Baraa <baraatv2006@gmail.com>
2026-07-20 02:41:08 +0300

    finalyyyyyyyyyyyyyyyyyyy (honesly i used some ai but as it is my first time usimg javascript ever , i used it

--- animal_info_qs.js
+++ animal_info_qs.js
@@ -6,10 +6,7 @@ let next = document.getElementById("next");
 let rightAnswers ;
 let wrongAnswer ;
 let selectedAnswer = "";
-let rananswer1 = Math.floor(Math.random() * 4 )
-let rananswer2 = Math.floor(Math.random() * 4 )
-let rananswer3 = Math.floor(Math.random() * 4 )
-let rananswer4 = Math.floor(Math.random() * 4 )
+
 let quiz = true;
 //console.log(rananswer1,rananswer2,rananswer3,rananswer4)
 //function randomAnswer (){
@@ -21,54 +18,74 @@ let quiz = true;
 
 
 
-
+/*
  if(rananswer2 == rananswer1){
     if(rananswer1 == 0){
         rananswer1++
     }else if(rananswer1 == 1){
-        rananswer1 - 1 || rananswer1 ++ || rananswer1 +2
+       rananswer1 = rananswer1 - 1 || rananswer1 ++ ||rananswer1 = rananswer1 +2
     }else if(rananswer1 == 2){
-        rananswer1 - 2 || rananswer1 - 1 || rananswer1 ++
+       rananswer1 = rananswer1 - 2 || rananswer1 = rananswer1 - 1 || rananswer1 ++
     }else if(rananswer1 == 3){
-        rananswer1 - 3 || rananswer1 - 2 || rananswer1 - 1
+       rananswer1 = rananswer1 - 3 || rananswer1 = rananswer1 - 2 ||rananswer1 = rananswer1 - 1
     }
  }
+*/
+let rananswer1 = Math.floor(Math.random() * 4);
+
+let rananswer2 = Math.floor(Math.random() * 4);
+while (rananswer2 === rananswer1) {
+    rananswer2 = Math.floor(Math.random() * 4);
+}
+
+let rananswer3 = Math.floor(Math.random() * 4);
+while (rananswer3 === rananswer1 || rananswer3 === rananswer2) {
+    rananswer3 = Math.floor(Math.random() * 4);
+}
+
+let rananswer4 = Math.floor(Math.random() * 4);
+while (
+    rananswer4 === rananswer1 ||
+    rananswer4 === rananswer2 ||
+    rananswer4 === rananswer3
+) {
+    rananswer4 = Math.floor(Math.random() * 4);
+}
 
 
 
 
 
-    if(rananswer1 == 0){
-        answer1.textContent = "Giraphe"
-    }else if(rananswer1 == 1){
-        answer2.textContent = "Bear"
-    }else if(rananswer1 == 2){
-        answer3.textContent = "Lion"
-    }else if(rananswer1 == 3){
-        answer4.textContent = "Elephant"
-    }
 
 
- 
 
-  
-    
- 
 
-  
-    
- 
 
-    if(rananswer2 == 0 && rananswer2 != rananswer1){
-        
-        answer1.textContent = "Bear"
-    }else if(rananswer2 == 1 && rananswer2 != rananswer1){
-        answer2.textContent = "Lion"
-    }else if(rananswer2 == 2 && rananswer2 != rananswer1){
-        answer3.textContent = "Elephant"
-    }else if(rananswer2 == 3 && rananswer2 != rananswer1){
-        answer4.textContent = "Giraphe"
-    }
+
+
+
+
+
+
+if (rananswer1 == 0) answer1.textContent = "Elephant";
+if (rananswer1 == 1) answer2.textContent = "Elephant";
+if (rananswer1 == 2) answer3.textContent = "Elephant";
+if (rananswer1 == 3) answer4.textContent = "Elephant";
+
+if (rananswer2 == 0) answer1.textContent = "Lion";
+if (rananswer2 == 1) answer2.textContent = "Lion";
+if (rananswer2 == 2) answer3.textContent = "Lion";
+if (rananswer2 == 3) answer4.textContent = "Lion";
+
+if (rananswer3 == 0) answer1.textContent = "Bear";
+if (rananswer3 == 1) answer2.textContent = "Bear";
+if (rananswer3 == 2) answer3.textContent = "Bear";
+if (rananswer3 == 3) answer4.textContent = "Bear";
+
+if (rananswer4 == 0) answer1.textContent = "Giraffe";
+if (rananswer4 == 1) answer2.textContent = "Giraffe";
+if (rananswer4 == 2) answer3.textContent = "Giraffe";
+if (rananswer4 == 3) answer4.textContent = "Giraffe";
 
 
 
