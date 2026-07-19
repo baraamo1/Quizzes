7b8c51c6aa3548628e965c5fe09a25301142dbbc
Baraa <baraatv2006@gmail.com>
2026-07-19 22:18:48 +0300

    some changes

--- animal_info_qs.js
+++ animal_info_qs.js
@@ -2,24 +2,56 @@ let answer1 = document.getElementById("answer1");
 let answer2 = document.getElementById("answer2");
 let answer3 = document.getElementById("answer3");
 let answer4 = document.getElementById("answer4");
+let next = document.getElementById("next");
 let rightAnswers ;
 let wrongAnswer ;
+let selectedAnswer = "";
+let answer = Math.floor(
+    Math.random()*4;
+)
+
+console.log(answer)
+
+
+answer1.onclick = function(){
+
+
+    selectedAnswer = "1";
+    console.log(selectedAnswer);
+}
+
+answer2.onclick = function(){
+
+    selectedAnswer = "2";
+        console.log(selectedAnswer);
+
+}
+answer3.onclick = function(){
+
+    selectedAnswer = "3";
+        console.log(selectedAnswer);
+}
+
+answer4.onclick = function(){
+
+    selectedAnswer = "4";
+        console.log(selectedAnswer);
+
+}
+
+
+
 function checkRightAnswer(){
 
-
-if(answer1.onclick){
-    wrongAnswer ++;
-}
-else if(answer2.onclick){
+  if(selectedAnswer == "2"){
     rightAnswers ++;
-}
-else if(answer3.onclick){
+    console.log("right")
+  }
+  else{
     wrongAnswer ++;
-}
-else if(answer4.onclick){
-    wrongAnswer ++;
+    console.log("wrong")
+  }
 
-}
 
 }
 
