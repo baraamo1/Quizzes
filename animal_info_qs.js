7eb8ffd6f358e423bd6b6d6450efc6951fd4fc06
Baraa <baraatv2006@gmail.com>
2026-07-20 08:31:00 +0300

    i finshed first question finalyyyyyyyyy

--- animal_info_qs.js
+++ animal_info_qs.js
@@ -3,13 +3,12 @@ let answer2 = document.getElementById("answer2");
 let answer3 = document.getElementById("answer3");
 let answer4 = document.getElementById("answer4");
 let next = document.getElementById("next");
-let rightAnswers ;
-let wrongAnswer ;
+let rightAnswers = 0;
+let wrongAnswer = 0;
 let selectedAnswer = "";
+let yourAnswer = document.getElementById("yourAnswer");
+let answer;
 
-let quiz = true;
-//console.log(rananswer1,rananswer2,rananswer3,rananswer4)
-//function randomAnswer (){
     
 
 
@@ -18,19 +17,7 @@ let quiz = true;
 
 
 
-/*
- if(rananswer2 == rananswer1){
-    if(rananswer1 == 0){
-        rananswer1++
-    }else if(rananswer1 == 1){
-       rananswer1 = rananswer1 - 1 || rananswer1 ++ ||rananswer1 = rananswer1 +2
-    }else if(rananswer1 == 2){
-       rananswer1 = rananswer1 - 2 || rananswer1 = rananswer1 - 1 || rananswer1 ++
-    }else if(rananswer1 == 3){
-       rananswer1 = rananswer1 - 3 || rananswer1 = rananswer1 - 2 ||rananswer1 = rananswer1 - 1
-    }
- }
-*/
+
 let rananswer1 = Math.floor(Math.random() * 4);
 
 let rananswer2 = Math.floor(Math.random() * 4);
@@ -57,91 +44,137 @@ while (
 
 
 
+if (rananswer1 == 0){ 
+    answer1.textContent = "Elephant";
+    answer = true;
+}
+
+if (rananswer1 == 1){
+    answer2.textContent = "Elephant";
+    answer = true;
+
+    }
+
+if (rananswer1 == 2){ 
+    answer3.textContent = "Elephant";
+    answer = true;
+
+}
+
+if (rananswer1 == 3){
+    answer4.textContent = "Elephant";
+    answer = true;
+    }
+    
+
+
+if (rananswer2 == 0){
+     answer1.textContent = "Lion";
+    }
+if (rananswer2 == 1){
+     answer2.textContent = "Lion";
+    }
+if (rananswer2 == 2){
+     answer3.textContent = "Lion";
+    
+    }
+if (rananswer2 == 3){ 
+    answer4.textContent = "Lion";
+}
+
+
+if (rananswer3 == 0){
+     answer1.textContent = "Bear";
+    }
+if (rananswer3 == 1){
+     answer2.textContent = "Bear";
+    }
+if (rananswer3 == 2){ 
+    answer3.textContent = "Bear";
+}
+if (rananswer3 == 3){ 
+    answer4.textContent = "Bear";
+
+}
+
+
+if (rananswer4 == 0){
+     answer1.textContent = "Giraffe";
+    }
+if (rananswer4 == 1){
+     answer2.textContent = "Giraffe";
+    }
+if (rananswer4 == 2){ 
+    answer3.textContent = "Giraffe";
+}
+if (rananswer4 == 3){
+     answer4.textContent = "Giraffe";
+    }
 
 
 
 
+/**/
+function checkRightAnswer1(){
+
+   yourAnswer.textContent = `Your Answer is : ${answer1.textContent}`
+}
 
 
+function checkRightAnswer2(){
+
+      yourAnswer.textContent = `Your Answer is : ${answer2.textContent}`
+
+}
 
 
+function checkRightAnswer3(){
+
+      yourAnswer.textContent = `Your Answer is : ${answer3.textContent}`
+
+}
+
+function checkRightAnswer4(){
+
+      yourAnswer.textContent = `Your Answer is : ${answer4.textContent}`
+
+}
 
 
-if (rananswer1 == 0) answer1.textContent = "Elephant";
-if (rananswer1 == 1) answer2.textContent = "Elephant";
-if (rananswer1 == 2) answer3.textContent = "Elephant";
-if (rananswer1 == 3) answer4.textContent = "Elephant";
-
-if (rananswer2 == 0) answer1.textContent = "Lion";
-if (rananswer2 == 1) answer2.textContent = "Lion";
-if (rananswer2 == 2) answer3.textContent = "Lion";
-if (rananswer2 == 3) answer4.textContent = "Lion";
-
-if (rananswer3 == 0) answer1.textContent = "Bear";
-if (rananswer3 == 1) answer2.textContent = "Bear";
-if (rananswer3 == 2) answer3.textContent = "Bear";
-if (rananswer3 == 3) answer4.textContent = "Bear";
-
-if (rananswer4 == 0) answer1.textContent = "Giraffe";
-if (rananswer4 == 1) answer2.textContent = "Giraffe";
-if (rananswer4 == 2) answer3.textContent = "Giraffe";
-if (rananswer4 == 3) answer4.textContent = "Giraffe";
-
-
-
-
-console.log(rananswer1,rananswer2)
-//}
-
-
-
-
-
+function NEXTfUNCTION (){
+    if(yourAnswer.textContent == "Your Answer is : Elephant"){
+        rightAnswers++
+    }else{
+        wrongAnswer++
+    }
+}
 
 
 
 
 /*
-answer1.onclick = function(){
 
+function checkRightAnswer (){
+   if (    answer1.textContent == "Elephant"){
+     rightAnswers++
+     console.log("1 is right")
+   }else    if (    answer2.textContent == "Elephant"){
+     rightAnswers++
+     console.log("2 is right")
 
-    selectedAnswer = "1";
-    console.log(selectedAnswer);
+   }else    if (    answer3.textContent == "Elephant"){
+     rightAnswers++
+     console.log("3 is right")
+
+   }else    if (    answer4.textContent == "Elephant"){
+     rightAnswers++
+     console.log("4 is right")
+
+   }else{
+    wrongAnswer++
+     console.log("wrong")
+
+   }
+   console.log(rightAnswers)
 }
-
-answer2.onclick = function(){
-
-    selectedAnswer = "2";
-        console.log(selectedAnswer);
-
-}
-answer3.onclick = function(){
-
-    selectedAnswer = "3";
-        console.log(selectedAnswer);
-}
-
-answer4.onclick = function(){
-
-    selectedAnswer = "4";
-        console.log(selectedAnswer);
-
-}
-
-
-
-function checkRightAnswer(){
-
-  if(selectedAnswer == "2"){
-    rightAnswers ++;
-    console.log("right")
-  }
-  else{
-    wrongAnswer ++;
-    console.log("wrong")
-  }
-
-
-}
-
-*/
+   */
